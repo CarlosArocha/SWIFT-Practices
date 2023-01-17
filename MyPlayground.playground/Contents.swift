e61058c28aaefeb396380eec7f82d8204736b47c
Carlos Arocha <carloslarochas@gmail.com>
2023-01-17 10:49:29 +0000

    First commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,27 @@
+//Don't change this code:
+func calculator() {
+  let a = Int(readLine()!)! //First input
+  let b = Int(readLine()!)! //Second input
+  
+  add(n1: a, n2: b)
+  subtract(n1: a, n2: b)
+  multiply(n1: a, n2: b)
+  divide(n1: a, n2: b)
+  
+}
+
+func add(n1 : Int, n2: Int) {
+    print(n1+n2)
+}
+
+func subtract(n1 : Int, n2: Int) {
+    print(n1-n2)
+}
+
+func multiply(n1 : Int, n2: Int) {
+    print(n1*n2)
+}
+
+func divide(n1 : Int, n2: Int) {
+    print(n1/n2)
+}
---
+++ Practice1/Practice1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practice1
+//
+//  Created by Carlos Arocha Saher on 17/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
